52b9247f4e4b930b437a981318050ee2886f39ae
victoriaplummer <victoria.plummer@webflow.com>
2025-09-12 03:22:03 -0400

    updated assets prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   base: "/careers-portal",
+  build: {
+    assetsPrefix: "/careers-portal",
+  },
   output: "server",
   security: { checkOrigin: false },
   adapter: cloudflare({
